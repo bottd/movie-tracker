6fe94e37286591a561359756d379cf26c788d626
Drake Bott <bott.da@gmail.com>
2019-01-11 13:45:03 -0700

    Updates

---
+++ precache-manifest.f76a91bc904c20c114f7871dadc6f2c0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0210b84c1f37cac96edb73e3621355a",
+    "url": "/movie-tracker/static/media/star.c0210b84.svg"
+  },
+  {
+    "revision": "25bcd45fd6c272607e1465a5e02a4582",
+    "url": "/movie-tracker/static/media/star-clear.25bcd45f.svg"
+  },
+  {
+    "revision": "05a753d921fc48588e3079d581b85d47",
+    "url": "/movie-tracker/static/media/movies.05a753d9.jpg"
+  },
+  {
+    "revision": "f8858d24fd028d581c979ccb1921428c",
+    "url": "/movie-tracker/static/media/film-red.f8858d24.svg"
+  },
+  {
+    "revision": "f5a723b953ab703507c1",
+    "url": "/movie-tracker/static/js/runtime~main.f5a723b9.js"
+  },
+  {
+    "revision": "acbeb23b8b84df387634",
+    "url": "/movie-tracker/static/js/main.acbeb23b.chunk.js"
+  },
+  {
+    "revision": "12fd8600131c81966f6e",
+    "url": "/movie-tracker/static/js/1.12fd8600.chunk.js"
+  },
+  {
+    "revision": "acbeb23b8b84df387634",
+    "url": "/movie-tracker/static/css/main.48408349.chunk.css"
+  },
+  {
+    "revision": "227316408dd040ee0e5935b5f5e7dc51",
+    "url": "/movie-tracker/index.html"
+  }
+];
